7bde0634f219753e61132ab549c42a733b640aaf
s.petrashka <piotrazsko@gmail.com>
2022-11-06 21:27:57 +0100

    started create prototype of CV

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-document.addEventListener("DOMContentLoaded", async () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const avatar = document.querySelector(".user_avatar");
+  const userName = document.querySelector(".user_name");
+  const email = document.querySelector(".email");
+
+  const getDataFromServer = async () => {
+    const data = await fetch("https://randomuser.me/api/", {
+      method: "GET",
+    });
+    const userData = await data.json();
+    const user = userData.results[0];
+    avatar.setAttribute("src", user.picture.large);
+    email.textContent = user.email;
+    userName.textContent = `${user.name.title} ${user.name.first} ${user.name.last}`;
+    return user;
+  };
+
+  const user = await getDataFromServer();
+});
