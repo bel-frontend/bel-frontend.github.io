61251f4cbc3252cc6ea4b2ab6fbe7bb57f8a8f67
loveJS <studina-natali@yandex.by>
2023-03-12 20:49:16 +0300

    added 404 page

---
+++ src/containers/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import notFound from './404.png';
+
+import style from './style.module.scss';
+const NotFound = () => (
+    <div className={style.contentWrap}>
+        <img src={notFound} title="not found" alt="not found" className={style.img}/>
+        <p>Тут нічога няма.</p>
+        <p>Паспрабуйце пашукаць што-небудзь іншае.</p>
+        <Link to="/" className={style.link}>
+            На галоўную
+        </Link>
+    </div>
+);
+
+export default NotFound;
