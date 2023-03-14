06d8f7e49daa63454350b7c469137dc105658f1c
loveJS <studina-natali@yandex.by>
2023-03-14 23:58:36 +0300

    added tags into the main and the article page

--- src/containers/Article/index.tsx
+++ src/containers/Article/index.tsx
@@ -39,7 +39,6 @@ const Article = ({
     const title = article?.meta?.title;
     const description = article?.meta?.description ?? article?.meta?.title;
 
-
     return article ? (
         <>
             <Helmet>
@@ -74,6 +73,10 @@ const Article = ({
             <main className="page__main main articlePage">
                 <article className="episode box">
                     <h2 className="episode__title">{article?.meta?.title}</h2>
+                    {article?.meta?.tags && article?.meta?.tags.map((tag: string) => (
+                            <span className={style.tag} key={tag}>#{tag}</span>
+                        )
+                    )}
                     <div className="content">
                         <MD>{article?.content}</MD>
                     </div>
