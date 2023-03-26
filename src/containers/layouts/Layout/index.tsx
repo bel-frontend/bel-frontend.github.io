e5af392dbf834afe2eb025b1ef33e18065ec1e8a
bel-frontend <goman.live.service@gmail.com>
2023-03-26 14:01:21 +0200

    v0.1.5

--- src/containers/layouts/Layout/index.tsx
+++ src/containers/layouts/Layout/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Container } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+
 import { Header, Footer } from 'components';
 import { ScrollToTop } from './components/ScrollToTop';
-import { Drawer } from './components/Drawer';
+// import { Drawer } from './components/Drawer';
 
 export const Layout = ({ children, ...props }: any) => {
     const {
@@ -10,7 +12,13 @@ export const Layout = ({ children, ...props }: any) => {
         route: { showHeader, showFooter = true, userIsAuth, maxWidth = 'md' },
         location,
         viewPort: { isMobile },
+        viewPort,
     } = props;
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
     return (
         <>
@@ -23,7 +31,7 @@ export const Layout = ({ children, ...props }: any) => {
                 />
             ) : null}
             {/* <Drawer history={history} userIsAuth={userIsAuth} /> */}
-            <ScrollToTop />
+            <ScrollToTop viewPort={viewPort} />
             <Container maxWidth={maxWidth} sx={{ minHeight: '80vh' }}>
                 {React.createElement(children, props)}
             </Container>
