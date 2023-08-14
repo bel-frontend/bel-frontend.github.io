ed40ee910d60a5e8b7c6952a7e2bb44bead5a10d
loveJS <studina-natali@yandex.by>
2023-08-14 21:51:09 +0300

    added pagination

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,37 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Box from '@mui/material/Box';
 import { useDispatch, useSelector } from 'react-redux';
+import Pagination from '@mui/material/Pagination';
+
 import {
-    getArticklesRequest,
     getArticklesSelector,
     searchArticle,
 } from 'modules/artickles';
-import { confirmCookiesAction } from 'modules/cookies';
-
 import { EpisodePreview } from './components/EpisodePreview/';
-import style from './style.module.scss';
 import { getCurrentUserSelector } from 'modules/auth';
 import { USER_ROLES } from 'constants/users';
 
+import style from './style.module.scss';
+
+const ARTICLES_PER_PAGE = 15;
+const DEFAULT_PAGE_NUM = 1;
+
 const Home = ({
-    route: { userIsAuth },
-    history,
-    location: { search },
-    ...props
-}: {
+                  route: { userIsAuth },
+                  history,
+                  location: { search },
+                  ...props
+              }: {
     route: { userIsAuth?: boolean };
     [key: string]: any;
 }) => {
+    const [pageNum, setPageNum] = useState(DEFAULT_PAGE_NUM);
     const dispatch = useDispatch();
     const currentUser: any = useSelector(getCurrentUserSelector);
-    React.useEffect(() => {
-        // dispatch(confirmCookiesAction());
-    }, []);
+
     React.useEffect(() => {
         const query = new URLSearchParams(search);
         const text = query.get('seacrhText');
-        dispatch(searchArticle(text));
-    }, [search, userIsAuth]);
+
+        let queryParams: { pageNum: number, pageSize: number, search?: string } = {
+            pageNum,
+            pageSize: ARTICLES_PER_PAGE,
+        };
+
+        if (text) {
+            queryParams = { search: text, ...queryParams };
+        }
+
+        dispatch(searchArticle(queryParams));
+    }, [search, userIsAuth, pageNum]);
 
     const { articles = [] }: any = useSelector(getArticklesSelector);
 
@@ -46,52 +58,59 @@ const Home = ({
         );
     }, [articles]);
 
-    return (
-        <>
-            <Box component={'main'} className={style.main}>
-                <a
-                    className={style.telegram}
-                    href="https://t.me/bel_frontend"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Далучайцеся да нашага Тэлеграм-канала
-                </a>
+    const onChangePaginationNumber = (e: ChangeEvent<unknown>, pageNumber: number) => {
+        setPageNum(pageNumber);
+    };
 
-                {preparedArticles &&
-                    preparedArticles.map(
-                        (
-                            {
-                                content,
-                                meta,
-                                id,
-                                isActive,
-                                likes,
-                            }: {
-                                content: string;
-                                meta: any;
-                                id: any;
-                                isActive: boolean;
-                                likes: any;
-                            },
-                            index: number,
-                        ) =>
-                            meta ? (
-                                <EpisodePreview
-                                    currentUser={currentUser}
-                                    history={history}
-                                    key={index}
-                                    userIsAuth={userIsAuth}
-                                    content={content}
-                                    meta={meta}
-                                    id={id}
-                                    isActive={isActive}
-                                    likes={likes}
-                                />
-                            ) : null,
-                    )}
-            </Box>
-        </>
+    return (
+        <Box component={'main'} className={style.main}>
+            <a
+                className={style.telegram}
+                href='https://t.me/bel_frontend'
+                target='_blank'
+                rel='noreferrer'
+            >
+                Далучайцеся да нашага Тэлеграм-канала
+            </a>
+
+            {!!preparedArticles.length &&
+                <>{preparedArticles.map(
+                    (
+                        {
+                            content,
+                            meta,
+                            id,
+                            isActive,
+                            likes,
+                        }: {
+                            content: string;
+                            meta: any;
+                            id: any;
+                            isActive: boolean;
+                            likes: any;
+                        },
+                        index: number,
+                    ) =>
+                        meta ? (
+                            <EpisodePreview
+                                currentUser={currentUser}
+                                history={history}
+                                key={index}
+                                userIsAuth={userIsAuth}
+                                content={content}
+                                meta={meta}
+                                id={id}
+                                isActive={isActive}
+                                likes={likes}
+                            />
+                        ) : null,
+                )}
+                    <Pagination count={ARTICLES_PER_PAGE} shape='rounded' variant='outlined'
+                                defaultPage={DEFAULT_PAGE_NUM}
+                                color='primary' onChange={onChangePaginationNumber} />
+                </>
+            }
+        </Box>
     );
 };
 
--- src/containers/layouts/Layout/index.tsx
+++ src/containers/layouts/Layout/index.tsx
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom';
 
 import { Header, Footer } from 'components';
 import { ScrollToTop } from './components/ScrollToTop';
-// import { Drawer } from './components/Drawer';
 
 export const Layout = ({ children, ...props }: any) => {
     const {
@@ -30,7 +29,6 @@ export const Layout = ({ children, ...props }: any) => {
                     userIsAuth={userIsAuth}
                 />
             ) : null}
-            {/* <Drawer history={history} userIsAuth={userIsAuth} /> */}
             <ScrollToTop viewPort={viewPort} />
             <Container maxWidth={maxWidth} sx={{ minHeight: '80vh' }}>
                 {React.createElement(children, props)}
