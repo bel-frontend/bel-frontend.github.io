ca30a8512e19fab175657f81c496fd3555a7edcc
bel-frontend <goman.live.service@gmail.com>
2024-10-30 08:23:03 +0100

    added debounce for getting news

--- src/modules/news/index.tsx
+++ src/modules/news/index.tsx
@@ -1,6 +1,7 @@
 import * as api_helpers from 'react_redux_api';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import React from 'react';
+import { debounce } from 'lodash';
 
 const modules = 'news';
 const {
@@ -15,10 +16,6 @@ const GET_GEMINI_NEWS_REQUEST = `${modules}/GET_GEMINI_NEWS_REQUEST`;
 
 const getOpenAiNewsRequest = actionCreator(GET_OPENAI_NEWS_REQUEST);
 const getGeminiNewsRequest = actionCreator(GET_GEMINI_NEWS_REQUEST);
-
-// export const addSubscribeRequest = actionCreator(SET_SUBSRIBE_REQUEST);
-// export const deleteSubscribeRequest = actionCreator(DISABLE_SUBSRIBES_REQUEST);
-// export const getSubscribeRequest = actionCreator(GET_SUBSRIBES_STATUS_REQUEST);
 
 apiRoutes.add(GET_OPENAI_NEWS_REQUEST, () => {
     return {
@@ -37,39 +34,46 @@ export const useGetNews = () => {
     const dispatch = useDispatch();
     const [isLoadingOpenAiNews, setIsLoadingOpenAiNews] = React.useState(false);
     const [isLoadingGeminiNews, setIsLoadingGeminiNews] = React.useState(false);
-    const getOpenAiNews = () =>
-        new Promise((resolve, reject) => {
-            setIsLoadingOpenAiNews(true);
-            dispatch(
-                getOpenAiNewsRequest(null, {
-                    onSuccess: (response: any) => {
-                        resolve(response);
-                        setIsLoadingOpenAiNews(false);
-                    },
-                    onFailure: (error: any) => {
-                        reject(error);
-                        setIsLoadingOpenAiNews(false);
-                    },
-                }),
-            );
-        });
 
-    const getGeminiNews = () =>
-        new Promise((resolve, reject) => {
-            setIsLoadingGeminiNews(true);
-            dispatch(
-                getGeminiNewsRequest(null, {
-                    onSuccess: (response: any) => {
-                        setIsLoadingGeminiNews(false);
-                        resolve(response);
-                    },
-                    onFailure: (error: any) => {
-                        setIsLoadingGeminiNews(false);
-                        reject(error);
-                    },
-                }),
-            );
-        });
+    const getOpenAiNews = debounce(
+        () =>
+            new Promise((resolve, reject) => {
+                setIsLoadingOpenAiNews(true);
+                dispatch(
+                    getOpenAiNewsRequest(null, {
+                        onSuccess: (response: any) => {
+                            resolve(response);
+                            setIsLoadingOpenAiNews(false);
+                        },
+                        onFailure: (error: any) => {
+                            reject(error);
+                            setIsLoadingOpenAiNews(false);
+                        },
+                    }),
+                );
+            }),
+        300,
+    );
+
+    const getGeminiNews = debounce(
+        () =>
+            new Promise((resolve, reject) => {
+                setIsLoadingGeminiNews(true);
+                dispatch(
+                    getGeminiNewsRequest(null, {
+                        onSuccess: (response: any) => {
+                            setIsLoadingGeminiNews(false);
+                            resolve(response);
+                        },
+                        onFailure: (error: any) => {
+                            setIsLoadingGeminiNews(false);
+                            reject(error);
+                        },
+                    }),
+                );
+            }),
+        300,
+    );
 
     return {
         getOpenAiNews,
